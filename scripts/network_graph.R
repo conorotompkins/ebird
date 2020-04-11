ac60bde58dd88baa21aa3c2720a2227940288338
Conor <conorotompkins@gmail.com>
2020-04-11 17:27:39 -0400

    reworked code. working on filtering by node/edge

--- scripts/network_graph.R
+++ scripts/network_graph.R
@@ -23,28 +23,56 @@ df <- vroom("data/ebd_US-PA-003_201001_202003_relFeb-2020.zip") %>%
   clean_names() %>% 
   mutate(observation_count = as.numeric(str_replace(observation_count, "X", as.character(NA))))
 
+df %>% 
+  distinct(county)
+
 first(df$observation_date)
 last(df$observation_date)
 
 df_counts <- df %>% 
-  count(common_name, name = "species_count", sort = TRUE)
+  count(common_name, name = "species_count", sort = TRUE) %>% 
+  top_n(5)
+
+df_counts
+
+df <- df %>% 
+  mutate(observation_event_id = str_c(observer_id, locality, observation_date, time_observations_started))
+
+
 
 df_pairs <- df %>% 
-  pairwise_count(common_name, sampling_event_identifier, wt = observation_count) %>% 
-  arrange(desc(n))
+  filter(common_name != "gull sp.") %>% 
+  pairwise_count(common_name, observation_event_id, wt = observation_count, diag = FALSE, upper = FALSE) %>% 
+  arrange(desc(n)) %>% 
+  drop_na(n) %>% 
+  filter(n >= 10)
+
+df_pairs <- df_pairs %>% 
+  filter(item1 == "Blue Jay" | item1 == "Northern Cardinal" | item2 == "Blue Jay" | item2 == "Northern Cardinal")
   
-df_pairs %>% 
-  slice(1:100) %>% 
-  mutate(n = log10(n)) %>% 
-  as_tbl_graph(directed = FALSE) %>% 
+graph_object <- df_pairs %>% 
+  as_tbl_graph(directed = FALSE)
+
+df_node_ids <- graph_object %>% 
   activate(nodes) %>% 
-  left_join(df_counts, by = c("name" = "common_name")) %>% 
+  mutate(id = row_number()) %>% 
+  as_tibble()
+
+target_node_id <- df_node_ids %>% 
+  filter(name == "Blue Jay" | name == "Northern Cardinal") %>% 
+  pull(id)
+
+
+graph_object %>% 
+  activate(edges) %>% 
+  filter(from %in% c(target_node_id)) %>%
   ggraph() +
     #geom_node_point() +
-    geom_edge_fan(aes(edge_width = n, alpha = n)) +
-    geom_node_label(aes(label = name, size = species_count)) +
-    scale_edge_width_continuous(range = c(.5, 3)) +
-    scale_edge_alpha_continuous(range = c(.2, 1)) +
-    scale_size_continuous(range = c(.4, 5)) +
-    #scale_node_size_continuous(range = c(.5, 2)) +
+    geom_edge_fan(aes(edge_width = n, 
+                      alpha = n,
+                      color = as.factor(from)
+    )) +
+    geom_node_point() +
+    #geom_node_label(aes(label = name)) +
+    scale_color_manual(values = c("red", "blue")) +
     theme_void()
