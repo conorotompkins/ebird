afedded443b42abe6363f7b66c665b5a29ad2c62
Conor <conorotompkins@gmail.com>
2020-04-18 11:54:06 -0400

    added filtering for protocol type

--- scripts/bar_plots.R
+++ scripts/bar_plots.R
@@ -23,7 +23,12 @@ df <- vroom("data/ebd_US-PA-003_201001_202003_relFeb-2020.zip", delim = "\t") %>
          observation_event_id = str_c(observer_id, locality, observation_date, time_observations_started)) %>% 
   filter(all_species_reported == 1)
 
+df_top_protocols <- df %>% 
+  count(protocol_type, sort = TRUE) %>% 
+  slice(1:2)
+
 df_counts <- df %>% 
+  semi_join(df_top_protocols) %>% 
   count(common_name, name = "species_count", sort = TRUE)
 
 df_counts
@@ -57,6 +62,7 @@ df_pair_count %>%
          y = NULL)
 
 df_pair_corr <- df %>% 
+  semi_join(df_top_protocols) %>% 
   pairwise_cor(common_name, observation_event_id, diag = FALSE, upper = FALSE)
 
 df_pair_corr %>% 
@@ -68,9 +74,9 @@ df_pair_corr %>%
   ungroup() %>% 
   mutate(item2 = reorder_within(x = item2, by = correlation, within = item1)) %>% 
   ggplot(aes(correlation, item2, fill = item1)) +
-  geom_col(alpha = .9) +
-  facet_wrap(~item1, scales = "free_y") +
-  scale_y_reordered() +
-  scale_fill_manual(values = c("blue", "red")) +
-  labs(x = "Correlation",
-       y = NULL)
+    geom_col(alpha = .9) +
+    facet_wrap(~item1, scales = "free_y") +
+    scale_y_reordered() +
+    scale_fill_manual(values = c("blue", "red")) +
+    labs(x = "Correlation",
+         y = NULL)
