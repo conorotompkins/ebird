13e453d010d27acab520cf260a3470306044abdd
Conor Tompkins <conorotompkins@gmail.com>
2024-10-14 16:50:43 -0400

    added futile tests of normality

--- cluster_tiles.R
+++ cluster_tiles.R
@@ -48,12 +48,12 @@ ebirdst_download_status(species = species_vec_all[3],
                         download_abundance = TRUE,
                         dry_run = TRUE)
 
-abundance_list <- pmap(list(species_vec_all, "abundance", "Pennsylvania"), pull_ebird_metrics)
-
-abundance_df <- list_rbind(abundance_list)
-
-abundance_df |>
-  write_csv("output/combined_ebird_metrics.csv")
+# abundance_list <- pmap(list(species_vec_all, "abundance", "Pennsylvania"), pull_ebird_metrics)
+# 
+# abundance_df <- list_rbind(abundance_list)
+# 
+# abundance_df |>
+#   write_csv("output/combined_ebird_metrics.csv")
 
 abundance_df <- read_csv("output/combined_ebird_metrics.csv")
 
@@ -83,36 +83,179 @@ occuring_species
 abundance_df <- abundance_df |> 
   semi_join(occuring_species)
 
+abundance_df |> 
+  filter(rel_abundance == max(rel_abundance))
+
+abundance_df |> 
+  filter(species_name == "Snow Goose") |> 
+  slice_sample(n = 5000) |> 
+  pull(rel_abundance) |> 
+  shapiro.test() |> 
+  pluck("p.value")
+
+test_normality <- function(x){
+    
+    x |> 
+      slice_sample(n = 5000, weight_by = rel_abundance) |> 
+      pull(rel_abundance) |> 
+      shapiro.test() |> 
+      purrr::pluck("p.value")
+  
+}
+
+test_normality <- safely(test_normality, NA)
+
+abundance_df |> 
+  filter(species_name == "American Goldfinch") |> 
+  slice_sample(n = 5000) |> 
+  ggplot(aes(rel_abundance)) +
+  geom_histogram()
+
+abundance_df |> 
+  filter(species_name == "American Goldfinch") |> 
+  test_normality() |> 
+  purrr::pluck("result")
+
+#shapiro-wilk
+norm_rs <- abundance_df |> 
+  group_nest(species_name) |> 
+  mutate(test = purrr::map(data, test_normality) |> purrr::map_dbl("result")) |> 
+  select(-data)
+
+#null hypothesis that data is normally distributed
+
+#evidence that data is NOT normally distributed
+norm_rs |> 
+  filter(test < .05)
+
+#least normal
+norm_rs |> filter(test == min(test, na.rm = TRUE))
+
+abundance_df |> 
+  filter(species_name == "Red-winged Blackbird") |> 
+  ggplot(aes(rel_abundance)) +
+  geom_histogram()
+
+#most normal
+norm_rs |> filter(test == max(test, na.rm = TRUE))
+
+abundance_df |> 
+  filter(species_name == "Dark-eyed Junco") |> 
+  ggplot(aes(rel_abundance)) +
+  geom_histogram()
+
+#cannot reject null
+norm_rs |> 
+  filter(test >= .05)
+
+norm_rs |> 
+  ggplot(aes(test)) +
+  geom_histogram() +
+  geom_vline(xintercept = .05)
+
+graph_normality_test <- function(x, species){
+  
+  x_summary <- x |> 
+    filter(species_name == species) |> 
+    group_by(species_name) |> 
+    summarize(mean = mean(rel_abundance), sd = sd(rel_abundance))
+  
+  res_sd1 <- x |> 
+    filter(species_name == species) |> 
+    left_join(x_summary) |> 
+    mutate(mean_plus_sd = mean + sd,
+           mean_minus_sd = mean - sd) |> 
+    mutate(test = between(rel_abundance, mean_minus_sd, mean_plus_sd)) |> 
+    summarize(mean(test) |> round(4)) |> 
+    pull()
+  
+  res_sd1_bool <- res_sd1 >= .68
+  
+  res_sd2 <- x |> 
+    filter(species_name == species) |> 
+    left_join(x_summary) |> 
+    mutate(mean_plus_sd = mean + (2 * sd),
+           mean_minus_sd = mean - (2 * sd)) |> 
+    mutate(test = between(rel_abundance, mean_minus_sd, mean_plus_sd)) |> 
+    summarize(mean(test) |> round(4)) |> 
+    pull()
+  
+  res_sd2_bool <- res_sd2 >= .95
+  
+  x |> 
+    filter(species_name == species) |> 
+    ggplot() +
+    geom_histogram(aes(rel_abundance)) +
+    geom_rect(data = x_summary,
+              aes(xmin = mean - sd, xmax = mean + sd, ymin = 0, ymax = Inf),
+              alpha = .3, fill = "red", color = NA) +
+    geom_rect(data = x_summary,
+              aes(xmin = mean - (2 * sd), xmax = mean + (2 * sd), ymin = 0, ymax = Inf),
+              alpha = .1, fill = "red", color = NA) +
+    geom_vline(data = x_summary, aes(xintercept = mean), color = "red") +
+    labs(title = str_c("Normality test of", species, sep = " "),
+         subtitle = str_c("68% of obs between mean +- 1 SD: ", res_sd1_bool, "(", res_sd1, ")", "\n",
+                          "95% of obs between mean +- 2 SD: ", res_sd2_bool,"(", res_sd2, ")",
+                          sep = ""))
+  
+}
+
+graph_normality_test(abundance_df, "Dark-eyed Junco")
+
 random_birds <- abundance_df |> 
   distinct(species_name) |> 
   slice_sample(n = 10)
 
+test_sp <- c("Dark-eyed Junco", "Field Sparrow", "Common Grackle", "Snow Goose")
+
 abundance_df |>
-  semi_join(random_birds) |> 
+  filter(species_name %in% test_sp) |> 
   ggplot(aes(x, y, fill = rel_abundance)) +
   geom_tile() +
   facet_wrap(vars(species_name)) +
   scale_fill_viridis_c()
 
+abundance_df |> 
+  filter(species_name %in% test_sp) |> 
+  ggplot(aes(rel_abundance)) +
+  geom_histogram() +
+  facet_wrap(vars(species_name), scales = "free", ncol = 1)
+
+abundance_df |> 
+  filter(species_name %in% test_sp) |> 
+  group_by(species_name) |> 
+  summarize(mean = mean(rel_abundance),
+            median = median(rel_abundance),
+            pct_95 = quantile(rel_abundance, .95),
+            pct_99 = quantile(rel_abundance, .99))
+
+abundance_df |> 
+  filter(species_name %in% test_sp) |> 
+  ggplot(aes(log10(rel_abundance + 1))) +
+  geom_histogram() +
+  facet_wrap(vars(species_name), scales = "free", ncol = 1)
+
 abundance_df <- abundance_df |> 
-  mutate(rel_abundance_scaled = scale(rel_abundance) |> as.numeric(), .by = species_name)
+  mutate(rel_abundance_scaled = rel_abundance |> scale() |> as.numeric(),
+         rel_abundance_scaled_log10 = log10(rel_abundance + 1) |> scale() |> as.numeric(), .by = species_name)
 
 abundance_df |>
-  semi_join(random_birds) |> 
+  filter(species_name %in% test_sp) |> 
   ggplot(aes(x, y, fill = rel_abundance_scaled)) +
   geom_tile() +
   facet_wrap(vars(species_name)) +
   scale_fill_viridis_c()
 
-abundance_df |> 
-  semi_join(random_birds) |> 
-  ggplot(aes(x = rel_abundance_scaled)) +
-  geom_density() +
-  facet_wrap(vars(species_name), scales = "free")
+abundance_df |>
+  filter(species_name %in% test_sp) |> 
+  ggplot(aes(x, y, fill = rel_abundance_scaled_log10)) +
+  geom_tile() +
+  facet_wrap(vars(species_name)) +
+  scale_fill_viridis_c()
 
 abundance_df_wide <- abundance_df |> 
-  select(-rel_abundance) |> 
-  pivot_wider(names_from = species_name, values_from = rel_abundance_scaled)
+  select(species_name, x, y, rel_abundance_scaled_log10) |> 
+  pivot_wider(names_from = species_name, values_from = rel_abundance_scaled_log10)
 
 abundance_df_noloc <- select(abundance_df_wide, -c(x, y))
 
@@ -190,7 +333,7 @@ p1 <- assignments |>
   facet_wrap(~ k)
 p1
 
-kclust <- kmeans(abundance_df_noloc, centers = 9)
+kclust <- kmeans(abundance_df_noloc, centers = 12)
 
 tidy(kclust) |> select(size, withinss, cluster) |> arrange(desc(size))
 
@@ -227,7 +370,7 @@ cluster_geo |>
 
 #hierarchical
 
-wss_plot <- fviz_nbclust(abundance_df_noloc, FUN = hcut, method = "wss", k.max = 20)
+wss_plot <- fviz_nbclust(abundance_df_noloc, FUN = hcut, method = "wss", k.max = 10)
 
 wss_plot
 
@@ -257,7 +400,7 @@ wss_plot$data |>
 #   plot()
 
 hc_spec <- hier_clust(
-  num_clusters = 8,
+  num_clusters = 7,
   linkage_method = "ward"
 )
 
